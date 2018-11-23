55ec3d31a4265905ce23090cc437d0130e36d0ce
Gyutae Oh <30851459+gtogto@users.noreply.github.com>
2018-11-23 15:47:33 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+# 이벤트 처리
+# Q버튼 생성 및 클릭 이벤트
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
@@ -8,6 +10,7 @@ class MyWindow(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        # self.pushButton 변수가 바인딩하고 있는 객체인 버튼에서 'clicked' 라는 시그널 발생
         self.pushButton.clicked.connect(self.btn_clicked)
 
     def btn_clicked(self):
